fd30020ebe7776bc1772d025377de0a109c07a7c
mani703 <mani703@naver.com>
2022-03-30 10:04:34 +0900

    io practice resolved

--- day10/src/com/bit/day10/Ex14.java
+++ day10/src/com/bit/day10/Ex14.java
@@ -9,23 +9,29 @@ import java.util.Date;
 public class Ex14 {
 	
 	public static void main(String[] args) {
-		String path = args[0];
+		String path = "C:\\";
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a hh:mm");
 		
 		File file = new File(path);
 		String[] msgs = file.list();
+		
+		int fileOfCount = 0;
+		int directoryOfCount = 0;
 		
 		for(int i=0; i<msgs.length; i++) {
 			File temp = new File(msgs[i]);
 			System.out.print(sdf.format(temp.lastModified()));
 			
 			if(temp.isDirectory()) {
+				directoryOfCount++;
 				System.out.print("\t<DIR>\t   ");
 			} else {
+				fileOfCount++;
 				System.out.print("\t\t" + temp.length());
 			}
-			
 			System.out.println("  " + temp.getName());
 		}
+		System.out.println("\t\t" + fileOfCount + "개 파일");
+		System.out.println("\t\t" + directoryOfCount + "개 디렉터리");
 	}
 }
