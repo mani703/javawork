fb8362e6c8674f64bff0be1bbcf003c1fd19e4e8
mani703 <mani703@naver.com>
2022-04-01 10:53:10 +0900

    Thread basic

---
+++ day13/src/com/bit/day13/Ex13.java
@@ -0,0 +1,26 @@
+package com.bit.day13;
+
+class Lec01 extends java.lang.Thread {
+	@Override
+	public void run() {
+		Thread thr = Thread.currentThread();
+		for(int i=0; i<50; i++) {
+			System.out.println(thr.getName() + ":" + i);
+		}
+	}
+}
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		Lec01 thr2 = new Lec01();
+		Lec01 thr3 = new Lec01();
+		thr2.start();
+		thr3.start();
+		Thread thr = Thread.currentThread();
+		for(int i=0; i<50; i++) {
+			System.out.println(thr.getName() + ":" + i);
+		}
+//		thr2.run();
+	}
+}
